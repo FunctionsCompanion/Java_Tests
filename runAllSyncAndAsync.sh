051d15064b0ffbd2386771c1cf20af7a6f7401f4
Chris Marino <christopher.marino@salesforce.com>
2022-10-22 10:14:15 -0700

    New org

--- runAllSyncAndAsync.sh
+++ runAllSyncAndAsync.sh
@@ -6,13 +6,13 @@ c=1
 remain=50
 rm *.log
 while [ $c -le $jobs ]; do 
-    sfdx force:apex:execute -f test_apex/runPLDAsync.apex -u test-mzczhjcguvwk@example.com >> outAsyncPLDA.log &
+    sfdx force:apex:execute -f test_apex/runPLDAsync.apex -u test-l7gbph0utd7u@example.com >> outAsyncPLDA.log &
     sleep .5
-    sfdx force:apex:execute -f test_apex/runPLDSync.apex -u test-mzczhjcguvwk@example.com  >> outSyncPLDA.log &
+    sfdx force:apex:execute -f test_apex/runPLDSync.apex -u test-l7gbph0utd7u@example.com  >> outSyncPLDA.log &
     sleep .5
-    sfdx force:apex:execute -f test_apex/runSFSDKAsync.apex -u test-mzczhjcguvwk@example.com >> outAsyncSFSDK.log &
+    sfdx force:apex:execute -f test_apex/runSFSDKAsync.apex -u test-l7gbph0utd7u@example.com >> outAsyncSFSDK.log &
     sleep .5
-    sfdx force:apex:execute -f test_apex/runSFSDKSync.apex -u test-mzczhjcguvwk@example.com >> outSyncSFSDK.log &
+    sfdx force:apex:execute -f test_apex/runSFSDKSync.apex -u test-l7gbph0utd7u@example.com >> outSyncSFSDK.log &
     sleep .5
     echo 'Another 4 runs that queue function invocations.' $c 'out of' $jobs 'batches started.' 
     c=$(($c+1))
